6039544c628b0f4ca653dfa3fac9852bee3c156f
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-08-27 16:30:04 +0100

    betty style for my_puts.c

--- my_puts.c
+++ my_puts.c
@@ -2,8 +2,8 @@
 
 /**
  * my_puts - output of characters and strings
+ * @str: pointer to string
  * Description:
- *
  * Return: the number of char printed
  * -1 if NULL
  */
@@ -17,7 +17,7 @@ int my_puts(char *str)
 		while (str[size] != '\0')
 			size++;
 		/* print the string and return size*/
-		return(write(1, str, size));
+		return (write(1, str, size));
 	}
 	return (-1);
 }
